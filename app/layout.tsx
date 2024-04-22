949a768be503bb47d4c038089de7f369a2f78e2b
cawauchi <tatsuya.kawauchi@photocreate.co.jp>
2024-04-22 21:10:42 +0900

    update: update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css';
+import { notoSans } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ja">
+      <body className={`${notoSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Noto_Sans, Lusitana } from 'next/font/google';
+
+export const notoSans = Noto_Sans({ subsets: ['latin'], weight: "400"});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: "400",
+  style: "normal",
+});
